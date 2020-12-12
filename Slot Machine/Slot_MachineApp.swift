e9118192a7f31bb76e35fc73aa3b2fee192b3aec
W Lawless <wlawless.sharpe@protonmail.com>
2020-12-12 12:42:16 -0500

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by W Lawless on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
